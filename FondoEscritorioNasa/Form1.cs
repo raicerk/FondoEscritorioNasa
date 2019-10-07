3d9011fd6ffceb3626e0841a66ecb3507aaf2e27
raicerk <raicerk@gmail.com>
2019-10-07 14:37:30 -0300

    Se agrega logica para descargar imagen desde la API de la NASA y cambiar el fondo de escritorio

--- FondoEscritorioNasa/Form1.cs
+++ FondoEscritorioNasa/Form1.cs
@@ -3,38 +3,45 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Security;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Script.Serialization;
 using System.Windows.Forms;
 
 namespace FondoEscritorioNasa
 {
     public partial class Form1 : Form
     {
+        static HttpClient client = new HttpClient();
         public Form1()
         {
             InitializeComponent();
         }
 
-        [SuppressUnmanagedCodeSecurity, SecurityCritical,
-
-        DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [SuppressUnmanagedCodeSecurity, SecurityCritical,DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
         public static extern bool SystemParametersInfo(int nAction,int nParam, string value, int ignore);
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://api.nasa.gov/planetary/apod?api_key=AytFla37ceqUfpdNgzH6szGYmiLeLWbGEh0n54Te");
+            var respuesta = req.GetResponse();
+            var dataStream = respuesta.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+            JavaScriptSerializer j = new JavaScriptSerializer();
+            Data model = j.Deserialize<Data>(responseFromServer);
+            using (WebClient client = new WebClient())
             {
-                if (openFileDialog1.ShowDialog() == DialogResult.OK)
-                {
-                    String wallpaper = openFileDialog1.FileName;
-                    //Mediante la API de windows actualizamos el fondo de escritorio pasando el path en la variable wallpaper
-                    SystemParametersInfo(20, 0, wallpaper, 0);
-                    //Mediante el registro de windows Actualizamos el valor que es el path del wallpaper
-                    //Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", RegistryKeyPermissionCheck.ReadWriteSubTree).SetValue("wallpaper", wallpaper);
-                }
+                client.DownloadFile(new Uri(model.hdurl), @"c:\temp\wallpaper.jpg");
+                SystemParametersInfo(20, 0, @"c:\temp\wallpaper.jpg", 0);
+                this.Close();
+                //Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", RegistryKeyPermissionCheck.ReadWriteSubTree).SetValue("wallpaper", wallpaper);
             }
         }
     }
